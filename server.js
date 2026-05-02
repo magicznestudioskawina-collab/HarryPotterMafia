64424c68c554d163c4a9c61de467c9b1cf0c78e2
magicznestudioskawina-collab <magiczne.studio.skawina@gmail.com>
2026-05-02 11:33:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,37 @@ const io = require('socket.io')(http, { cors: { origin: "*" } });
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-    console.log('Nowy czarodziej w zamku!');
+let gameStarted = false;
 
+io.on('connection', (socket) => {
     socket.on('start-game', () => {
+        if (gameStarted) return; // Zapobiega ponownemu losowaniu w trakcie gry
+        
+        gameStarted = true;
         const allSockets = Array.from(io.sockets.sockets.values());
         const count = allSockets.length;
 
-        // Dynamiczny balans ról
+        // Balans ról Studio Skawina
         let gameRoles = [];
         if (count >= 1) gameRoles.push('Harry Potter (Auror)');
         if (count >= 2) gameRoles.push('Lord Voldemort (Lider Mafii)');
         if (count >= 3) gameRoles.push('Ron Weasley (Straznik)');
         if (count >= 4) gameRoles.push('Smierciozerca (Mafia)');
-        if (count >= 5) gameRoles.push('Hermiona Granger (Logika)');
-        if (count >= 6) gameRoles.push('Severus Snape (Szpieg)');
         
-        // Reszta to uczniowie
-        while (gameRoles.length < count) {
-            gameRoles.push('Uczeń Hogwartu');
-        }
-
+        while (gameRoles.length < count) gameRoles.push('Uczeń Hogwartu');
         let shuffled = gameRoles.sort(() => 0.5 - Math.random());
-        
-        // Sygnał do dźwięku dla urządzenia, które kliknęło START
-        socket.emit('play-sound');
 
         allSockets.forEach((s, index) => {
             s.emit('assign-role', shuffled[index]);
         });
+
+        // Powiadomienie o starcie nocy do wszystkich
+        io.emit('night-started');
+    });
+
+    socket.on('reset-game', () => {
+        gameStarted = false;
+        io.emit('game-reset');
     });
 });
 
